cb8462ab5c5e2dab8ce29c35ec61f8d7894638df
MrszhuL <57440504+MrszhuL@users.noreply.github.com>
2022-07-24 11:49:17 +0800

    specify the range of data

--- DataGenerator/generator.py
+++ DataGenerator/generator.py
@@ -2,19 +2,52 @@ import json
 import random
 import time
 
-
 class Gene:
     def __init__(self):
-        data = dict()
-        data['Temperature'] = random.randint(1, 100)
-        data['Displacement'] = random.randint(1, 100)
-        data['Humidity'] = random.randint(1, 100)
-        data['Pressure'] = random.randint(1, 100)
-        data['FlowRate'] = random.randint(1, 100)
+        self.data = dict()
+        self.data['Temperature'] = 30 #####realTemperature
+        self.data['Displacement'] = 44##############fake below
+        self.data['Humidity'] = 33
+        self.data['Pressure'] = 66
+        self.data['FlowRate'] = 55
+        
+    def TempException(self,excep=False):
+        if excep==False:
+            return
+        self.data['Temperature']=self.data['Temperature']+random.normalvariate(0,0.6)
+    
+    def DisException(self,excep=False):
+        if excep==False:
+            return
+        self.data['Displacement']=self.data['Displacement']+random.normalvariate(0,0.6)
+
+    def HumException(self,excep=False):
+        if excep==False:
+            return
+        self.data['Humidity']=self.data['Humidity']+random.normalvariate(0,0.6)
+    
+    def PressException(self,excep=False):
+        if excep==False:
+            return
+        self.data['Pressure']=self.data['Pressure']+random.normalvariate(0,0.6)
+    
+    def FlowException(self,excep=False):
+        if excep==False:
+            return
+        self.data['FlowRate']=self.data['FlowRate']+random.normalvariate(0,0.6)
+    
+
+    def __del__(self):
         with open(r"./DataGenerator/data.json", 'w') as f:
-            json.dump(data, f)
+            json.dump(self.data, f)
+
 
 
 while 1:
-    Gene()
+    ok=Gene()
+    ok.TempException(random.randint(1,14)%3==1)
+    ok.DisException(random.randint(1,14)%3==1)
+    ok.FlowException(random.randint(1,14)%3==1)
+    ok.HumException(random.randint(1,14)%3==1)
+    ok.PressException(random.randint(1,14)%3==1)
     time.sleep(1)
