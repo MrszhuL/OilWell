4d94a6f968340d3a806abe1204c78d87482c5257
MrszhuL <57440504+MrszhuL@users.noreply.github.com>
2022-07-24 11:45:14 +0800

    beautify the picture

--- DataAnalyse/analyse.py
+++ DataAnalyse/analyse.py
@@ -12,23 +12,27 @@ disp = []
 setinterval = [22, 77]  ###################################
 
 
-def plott(temper, name):
+def plott(temper,name,SetInterval=[29,31]):#################change
     plt.cla()
+    plt.ylim(SetInterval[0]-3,SetInterval[1]+3)
     plt.ion()
     plt.plot(temper)
-    plt.axhline(float(sum(temper) / len(temper)), label='mean', color='g')
-    minn = np.argmin(temper)
-    plt.annotate('[' + str(minn) + ', ' + str(temper[minn]) + ']', xy=(minn, temper[minn]), xytext=(minn, temper[minn]))
-    maxx = np.argmax(temper)
-    plt.annotate('[' + str(maxx) + ', ' + str(temper[maxx]) + ']', xy=(maxx, temper[maxx]), xytext=(maxx, temper[maxx]))
-    plt.text(0, float(sum(temper) / len(temper)), f'{round(sum(temper) / len(temper), 4)}(mean)')
-    red = [(j, i) for j, i in enumerate(temper) if i < setinterval[0] or i > setinterval[1]]
-    for i, j in red:
-        plt.plot(i, j, color='r', marker='o')
-    plt.plot(minn, temper[minn], 'ko')
-    plt.plot(maxx, temper[maxx], 'ko')
+    plt.axhline(float(sum(temper)/len(temper)),label=f'mean: {round(sum(temper)/len(temper),4)}',color='g')
+    plt.axhline(SetInterval[0],linestyle='--',color='r',label='interval')
+    plt.axhline(SetInterval[1],linestyle='--',color='r')
+    minn=np.argmin(temper)
+    plt.annotate('['+str(minn)+', '+str(round(temper[minn],4))+']',xy=(minn,temper[minn]),xytext=(minn,temper[minn]))
+    maxx=np.argmax(temper)
+    plt.annotate('['+str(maxx)+', '+str(round(temper[maxx],4))+']',xy=(maxx,temper[maxx]),xytext=(maxx,temper[maxx]))
+    red=[(j,i) for j,i in enumerate(temper) if i<SetInterval[0] or i>SetInterval[1]]
+    for i,j in red:
+        plt.plot(i,j,color='r',marker='o')
+        plt.annotate('['+str(i)+', '+str(round(j,4))+']',xy=(i,j),xytext=(i,j)) 
+    plt.plot(minn,temper[minn],'ko') 
+    plt.plot(maxx,temper[maxx],'ko')
     plt.xlabel('Time')
     plt.ylabel(name)
+    plt.legend()
     plt.pause(0.1)
     plt.show()
     plt.savefig(r'./{name:}.png'.format(name=name))
