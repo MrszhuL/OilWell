264ce34c9d51f040bd7eaaf3bdf4f57c95b2fd95
MrszhuL <2837815584@qq.com>
2022-08-18 10:24:21 +0800

    add DataAnalyse/gifs

--- DataAnalyse/analyse.py
+++ DataAnalyse/analyse.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import imageio.v2 as imageio
 
 temper = []
 flow = []
@@ -10,35 +11,43 @@ press = []
 humid = []
 disp = []
 setinterval = [22, 77]  ###################################
+pic = []
 
 
-def plott(temper,name,SetInterval=[29,31]):#################change
+def plott(temper, name, SetInterval=[29, 31]):  #################change
     plt.cla()
-    plt.ylim(SetInterval[0]-3,SetInterval[1]+3)
+    plt.ylim(SetInterval[0] - 3, SetInterval[1] + 3)
     plt.ion()
     plt.plot(temper)
-    plt.axhline(float(sum(temper)/len(temper)),label=f'mean: {round(sum(temper)/len(temper),4)}',color='g')
-    plt.axhline(SetInterval[0],linestyle='--',color='r',label='interval')
-    plt.axhline(SetInterval[1],linestyle='--',color='r')
-    minn=np.argmin(temper)
-    plt.annotate('['+str(minn)+', '+str(round(temper[minn],4))+']',xy=(minn,temper[minn]),xytext=(minn,temper[minn]))
-    maxx=np.argmax(temper)
-    plt.annotate('['+str(maxx)+', '+str(round(temper[maxx],4))+']',xy=(maxx,temper[maxx]),xytext=(maxx,temper[maxx]))
-    red=[(j,i) for j,i in enumerate(temper) if i<SetInterval[0] or i>SetInterval[1]]
-    for i,j in red:
-        plt.plot(i,j,color='r',marker='o')
-        plt.annotate('['+str(i)+', '+str(round(j,4))+']',xy=(i,j),xytext=(i,j)) 
-    plt.plot(minn,temper[minn],'ko') 
-    plt.plot(maxx,temper[maxx],'ko')
+    plt.axhline(float(sum(temper) / len(temper)), label=f'mean: {round(sum(temper) / len(temper), 4)}', color='g')
+    plt.axhline(SetInterval[0], linestyle='--', color='r', label='interval')
+    plt.axhline(SetInterval[1], linestyle='--', color='r')
+    minn = np.argmin(temper)
+    plt.annotate('[' + str(minn) + ', ' + str(round(temper[minn], 4)) + ']', xy=(minn, temper[minn]),
+                 xytext=(minn, temper[minn]))
+    maxx = np.argmax(temper)
+    plt.annotate('[' + str(maxx) + ', ' + str(round(temper[maxx], 4)) + ']', xy=(maxx, temper[maxx]),
+                 xytext=(maxx, temper[maxx]))
+    red = [(j, i) for j, i in enumerate(temper) if i < SetInterval[0] or i > SetInterval[1]]
+    for i, j in red:
+        plt.plot(i, j, color='r', marker='o')
+        plt.annotate('[' + str(i) + ', ' + str(round(j, 4)) + ']', xy=(i, j), xytext=(i, j))
+    plt.plot(minn, temper[minn], 'ko')
+    plt.plot(maxx, temper[maxx], 'ko')
     plt.xlabel('Time')
     plt.ylabel(name)
     plt.legend()
     plt.pause(0.1)
     plt.show()
     plt.savefig(r'./{name:}.png'.format(name=name))
+    ##########################################################################
+
+    pic.append(imageio.imread(r'./{name:}.png'.format(name=name)))
 
 
-while 1:
+count = 20
+while count >= 1:
+    count = count - 1
     with open(r"../DataGenerator/data.json", 'r') as f:
         data = json.load(f)
     time.sleep(1)
@@ -51,3 +60,5 @@ while 1:
     except:
         pass
     plott(temper, 'Temperature')
+
+imageio.mimsave("p3.gif", pic, fps=5)
--- DataGenerator/generator.py
+++ DataGenerator/generator.py
@@ -2,52 +2,51 @@ import json
 import random
 import time
 
+
 class Gene:
     def __init__(self):
         self.data = dict()
-        self.data['Temperature'] = 30 #####realTemperature
-        self.data['Displacement'] = 44##############fake below
+        self.data['Temperature'] = 30  #####realTemperature
+        self.data['Displacement'] = 44  ##############fake below
         self.data['Humidity'] = 33
         self.data['Pressure'] = 66
         self.data['FlowRate'] = 55
-        
-    def TempException(self,excep=False):
-        if excep==False:
-            return
-        self.data['Temperature']=self.data['Temperature']+random.normalvariate(0,0.6)
-    
-    def DisException(self,excep=False):
-        if excep==False:
-            return
-        self.data['Displacement']=self.data['Displacement']+random.normalvariate(0,0.6)
 
-    def HumException(self,excep=False):
-        if excep==False:
+    def TempException(self, excep=False):
+        if excep == False:
             return
-        self.data['Humidity']=self.data['Humidity']+random.normalvariate(0,0.6)
-    
-    def PressException(self,excep=False):
-        if excep==False:
+        self.data['Temperature'] = self.data['Temperature'] + random.normalvariate(0, 0.6)
+
+    def DisException(self, excep=False):
+        if excep == False:
             return
-        self.data['Pressure']=self.data['Pressure']+random.normalvariate(0,0.6)
-    
-    def FlowException(self,excep=False):
-        if excep==False:
+        self.data['Displacement'] = self.data['Displacement'] + random.normalvariate(0, 0.6)
+
+    def HumException(self, excep=False):
+        if excep == False:
             return
-        self.data['FlowRate']=self.data['FlowRate']+random.normalvariate(0,0.6)
-    
+        self.data['Humidity'] = self.data['Humidity'] + random.normalvariate(0, 0.6)
+
+    def PressException(self, excep=False):
+        if excep == False:
+            return
+        self.data['Pressure'] = self.data['Pressure'] + random.normalvariate(0, 0.6)
+
+    def FlowException(self, excep=False):
+        if excep == False:
+            return
+        self.data['FlowRate'] = self.data['FlowRate'] + random.normalvariate(0, 0.6)
 
     def __del__(self):
         with open(r"./DataGenerator/data.json", 'w') as f:
             json.dump(self.data, f)
 
 
-
 while 1:
-    ok=Gene()
-    ok.TempException(random.randint(1,14)%3==1)
-    ok.DisException(random.randint(1,14)%3==1)
-    ok.FlowException(random.randint(1,14)%3==1)
-    ok.HumException(random.randint(1,14)%3==1)
-    ok.PressException(random.randint(1,14)%3==1)
+    ok = Gene()
+    ok.TempException(random.randint(1, 14) % 3 == 1)
+    ok.DisException(random.randint(1, 14) % 3 == 1)
+    ok.FlowException(random.randint(1, 14) % 3 == 1)
+    ok.HumException(random.randint(1, 14) % 3 == 1)
+    ok.PressException(random.randint(1, 14) % 3 == 1)
     time.sleep(1)
